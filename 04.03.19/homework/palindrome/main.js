6aa8ac93d9fc0bdc01777c00cb067baf2466330f
enextus <enextus@gmail.com>
2019-03-05 18:04:01 +0300

    palindrome check

---
+++ 04.03.19/homework/palindrome/main.js
@@ -0,0 +1,33 @@
+var strOne = null;
+var strTwo = null;
+
+function delSpaces(str){
+  str = str.replace(/\s/g, ''); //delete the spaces
+  return str
+}
+
+function getRevert(str) {
+  str = delSpaces(str);
+  str = str.split(""); //convert str to array
+  str = str.reverse(); //revers the arrays elements
+  str = str.join(""); //join the values together
+	return str;
+}
+
+function getCheckPalindrome(strOne, strTwo) {
+  strOne = delSpaces(strOne);
+  if (strOne == strTwo){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var userInput = prompt("Please enter the word/words to check palindrome.");
+var revertInput = getRevert(userInput);
+
+if (getCheckPalindrome(userInput, revertInput)){
+    alert(":) This was a palindrome!");
+  } else {
+    alert(":( This was't a palindrome!");
+}
