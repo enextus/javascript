457620fbef6ebf4fd815ce79cc95425d5fe27b12
enextus <enextus@gmail.com>
2019-03-05 16:33:31 +0300

    classwork

---
+++ 04.03.19/classwork/main.js
@@ -0,0 +1,43 @@
+Sergey Arzamasov, [04.03.19 20:14]
+var people = [
+  { sex: 'M', name: 'Andrew' },
+  { sex: 'M', name: 'John' },
+  { sex: 'W', name: 'Anna' },
+  { sex: 'M', name: 'Andrew' },
+  { sex: 'M', name: 'Ivan' },
+  { sex: 'N', name: 'Eduardo' },
+  { sex: 'W', name: 'Kate' },
+];
+
+var men = [];
+var women = [];
+var none = [];
+
+for (var i = 0; i < people.length; i++) {
+  if (people[i].sex === 'M') {
+    men.push(people[i]);
+  } else if (people[i].sex === 'W') {
+    women.push(people[i]);
+  } else {
+    none.push(people[i]);
+  }
+}
+
+Sergey Arzamasov, [04.03.19 22:11]
+// function declaration
+function getPersonAge(person) {
+  if (person && person.age) {
+    return person.age;
+  }
+
+  return 18;
+}
+
+// function expression
+var getPersonAge = function (person) {
+  if (person && person.age) {
+    return person.age;
+  }
+
+  return 18;
+};
