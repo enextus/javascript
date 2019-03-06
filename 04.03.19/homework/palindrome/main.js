8ad2640887342a7e9bffe7cccba5428cf9ebb767
enextus <enextus@gmail.com>
2019-03-06 13:34:01 +0300

    some changes

--- 04.03.19/homework/palindrome/main.js
+++ 04.03.19/homework/palindrome/main.js
@@ -2,19 +2,33 @@ var strOne = null;
 var strTwo = null;
 var str = null;
 
-function delSpaces(str){
+function delSpaces(str) {
   str = str.replace(/\s/g, ''); //delete the spaces
   return str
 }
 
+/*
 function getRevert(str) {
   str = delSpaces(str);
 	return str.split("").str.reverse().str.join("");
 }
+*/
+
+function getReverse(str) {
+  str = delSpaces(str);
+  var newStr = '';
+  var i = 0;
+
+  for (var i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+
+  return newStr;
+}
 
 function getCheckPalindrome(strOne, strTwo) {
   strOne = delSpaces(strOne);
-  if (strOne == strTwo){
+  if ( strOne == strTwo ) {
     return true;
     } else {
       return false;
@@ -22,9 +36,9 @@ function getCheckPalindrome(strOne, strTwo) {
 }
 
 var userInput = prompt("Please enter the word/words to check palindrome.");
-var revertInput = getRevert(userInput);
+var revertInput = getReverse(userInput);
 
-if (getCheckPalindrome(userInput, revertInput)){
+if ( getCheckPalindrome(userInput, revertInput) ) {
     alert(":) This was a palindrome!");
     } else {
       alert(":( This was't a palindrome!");
---
+++ 04.03.19/homework/reverse/main.js
@@ -0,0 +1,27 @@
+var str = null;
+
+/*
+function getReverse(str) {
+  str = str.split("");
+  str = str.reverse();
+  str = str.join("");
+	return str;
+}
+*/
+
+function getReverse(str) {
+  var newStr = '';
+  var i = 0;
+
+  for (var i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+
+  return newStr;
+}
+
+
+var userInput = prompt("Please enter the word/words to revert its.");
+var reverseInput = getReverse(userInput);
+
+alert("Here is your reverted thing: " + reverseInput);
