d31804475f1bac8c189bf32571c025b5fc6d7cb2
enextus <enextus@gmail.com>
2019-03-08 19:16:42 +0300

    refactored

--- 07.03.19/homework/conversionCelsiusToFahrenheit/main.js
+++ 07.03.19/homework/conversionCelsiusToFahrenheit/main.js
@@ -3,18 +3,18 @@ var fahrenheit = null;
 
 function convertToFahrenheit( celsius ) {
 
-  if ( !isNaN(celsius) ) {
+  if ( !isNaN( celsius ) ) {
 
     var fahrenheit = celsius * 9 / 5 + 32;
 
-    return fahrenheit + ' \xB0F.';
+    return fahrenheit + ' \xB0F';
   } else {
 
     return 0;
   }
 }
 
-var celsius = parseInt(prompt("Please enter the worth of celsius temperature to convert it in to fahrenheit."), 10);
+var celsius = parseInt( prompt( "Please enter the worth of celsius temperature to convert it in to fahrenheit." ), 10 );
 
 var result = convertToFahrenheit( celsius );
-console.log(result);
+console.log( result );
--- 07.03.19/homework/conversionFahrenheitToCelsius/main.js
+++ 07.03.19/homework/conversionFahrenheitToCelsius/main.js
@@ -5,16 +5,16 @@ function convertToFahrenheit( fahrenheit ) {
 
   if ( !isNaN(fahrenheit) ) {
 
-    var celsius = ((fahrenheit - 32) * 5 / 9 ).toFixed(1);
+    var celsius = ( ( fahrenheit - 32 ) * 5 / 9 ).toFixed( 1 );
 
-    return celsius + '\xB0C';
+    return celsius + ' \xB0C';
   } else {
 
     return 0;
   }
 }
 
-var fahrenheit = parseInt(prompt("Please enter the worth of fahrenheit temperature to convert it in to celsius."), 10);
+var fahrenheit = parseInt( prompt( "Please enter the worth of fahrenheit temperature to convert it in to celsius." ), 10 );
 
 var result = convertToFahrenheit( fahrenheit );
-console.log(result);
+console.log( result );
