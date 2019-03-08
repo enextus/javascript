de8b40c297103bf64e401ad9b8d660f0b9aa5c44
enextus <enextus@gmail.com>
2019-03-08 18:31:36 +0300

    temperatur

---
+++ 07.03.19/homework/conversionCelsiusToFahrenheit/main.js
@@ -0,0 +1,20 @@
+var celsius = null;
+var fahrenheit = null;
+
+function convertToFahrenheit( celsius ) {
+
+  if ( !isNaN(celsius) ) {
+
+    var fahrenheit = celsius * 9 / 5 + 32;
+
+    return fahrenheit + ' \xB0F.';
+  } else {
+
+    return 0;
+  }
+}
+
+var celsius = parseInt(prompt("Please enter the worth of celsius temperature to convert it in to fahrenheit."), 10);
+
+var result = convertToFahrenheit( celsius );
+console.log(result);
---
+++ 07.03.19/homework/conversionFahrenheitToCelsius/main.js
@@ -0,0 +1,20 @@
+var celsius = null;
+var fahrenheit = null;
+
+function convertToFahrenheit( fahrenheit ) {
+
+  if ( !isNaN(fahrenheit) ) {
+
+    var celsius = ((fahrenheit - 32) * 5 / 9 ).toFixed(1);
+
+    return celsius + '\xB0C';
+  } else {
+
+    return 0;
+  }
+}
+
+var fahrenheit = parseInt(prompt("Please enter the worth of fahrenheit temperature to convert it in to celsius."), 10);
+
+var result = convertToFahrenheit( fahrenheit );
+console.log(result);
