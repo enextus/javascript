d64f25a517b86025357e5d29afe7400cc3e8d9bd
enextus <enextus@gmail.com>
2019-03-09 16:04:46 +0300

    myforEachFunc refactoring

--- 07.03.19/homework/myforEachFunc/main.js
+++ 07.03.19/homework/myforEachFunc/main.js
@@ -1,14 +1,18 @@
 var myArr = [1, 2, 3, 'a', 'b', 'c', NaN, true, [false, true, 1, 3]];
 
 function myFunction ( elem, index ) {
-  console.log( index + ". element of array is " + elem );
+  console.log( ( index + 1 ) + ". element of array is " + elem );
   }
 
 function myforEachFunc( myArr, myFunction ) {
 
-  var result = myArr.forEach(myFunction);
+    // myArr.forEach(myFunction);
 
-  return result;
+    for (index = 0; index < myArr.length; ++index) {
+
+      myFunction( myArr[index], index );
+
+    }
   }
 
 console.log( myforEachFunc( myArr, myFunction ) );
