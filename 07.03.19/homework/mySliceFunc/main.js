c62f9cc83d19641a6cba83f8a363f390d24eb16b
enextus <enextus@gmail.com>
2019-03-14 11:53:51 +0300

    func2

--- 07.03.19/homework/mySliceFunc/main.js
+++ 07.03.19/homework/mySliceFunc/main.js
@@ -12,7 +12,7 @@ function mySliceFunc(arr, startElemNum, endElemNum) {
 
   function myLoop(arr, start, end) {
     // console.log('arr = ' + arr + ', ' + 'start = ' + start + ', ' + 'end = ' + end);
-    for (var index = start; index < arr.length; ++index) {
+    for (var index = start; index < arr.length; index++) {
       myNewArray.push(arr[index]);
 
       if (index === end) {
@@ -24,15 +24,21 @@ function mySliceFunc(arr, startElemNum, endElemNum) {
 
   if (!isNaN(startElemNum) && !isNaN(endElemNum) && (typeof startElemNum === 'number') && (typeof endElemNum === 'number')) {
 
+    //1. case: (myArr, 0, 1) (myArr, 0, 0) (myArr, 1, 0) (myArr, 1, -1) (myArr, -1, 1)  (myArr, 1, 1) (myArr, -1, -1) OK
+
+    console.log('//1. case, (myArr, 1, -1) (myArr, -1, 1)  (myArr, 1, 1) (myArr, -1, -1)');
+
     if (endElemNum > 0) {
-
       end = endElemNum - 1;
-
     } else if (endElemNum < 0 && (arr.length + endElemNum) > 0) {
 
-      end = arr.length - 1 + endElemNum;
+      console.log('here1');
+      end = arr.length + endElemNum;
+      console.log('start = ' + start + ', end = ' + end);
 
     } else if (arr.length + endElemNum <= 0) {
+
+      console.log('here2');
 
       return myNewArray;
     }
@@ -43,22 +49,27 @@ function mySliceFunc(arr, startElemNum, endElemNum) {
       return myNewArray;
     }
 
+    console.log('start = ' + start + 'end = ' + end);
     myNewArray = myLoop(arr, start, end);
     return myNewArray;
 
   } else if (typeof startElemNum === 'undefined' && typeof endElemNum === 'undefined') {
 
+    // 2. case: (myArr) OK
+    console.log('2. case: (myArr)');
     myNewArray = arr;
 
   } else if ( !isNaN(startElemNum) && (typeof startElemNum === 'number') && !endElemNum ) {
 
+    // 3. case: (myArr, 0) (myArr, 1) (myArr, -1)
+    console.log('3. case: (myArr, 0) (myArr, 1) (myArr, -1)');
     if (startElemNum >= 0) {
       start = startElemNum;
     } else if  (startElemNum < 0) {
       arr.length + startElemNum >= 0 ? start = arr.length + startElemNum : start = 0;
     }
 
-    for (var index = start; index < arr.length; ++index) {
+    for (var index = start; index < arr.length; index++) {
       myNewArray.push(arr[index]);
 
       if (index === arr.length) {
@@ -67,8 +78,10 @@ function mySliceFunc(arr, startElemNum, endElemNum) {
     }
   }
 
-  return myNewArray;
+  // case: (myArr, "sdfsdf") (myArr, "sdfsdf", "sdfsdf") ОК
+  console.log('myArr = ' + myArr);
+  return myArr;
 }
 
-var result = mySliceFunc(myArr, 4, 18);
+var result = mySliceFunc(myArr, -1);
 console.log(result);
