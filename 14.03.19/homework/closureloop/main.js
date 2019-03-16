01964be022ac4eca2ee756cdc270a578d4d39e5e
enextus <enextus@gmail.com>
2019-03-16 11:28:22 +0300

    closureloop

---
+++ 14.03.19/homework/closureloop/main.js
@@ -0,0 +1,17 @@
+function sum(firstOperand) {
+
+    var currentSum = firstOperand;
+
+    function f(secondOperand) {
+        currentSum += secondOperand;
+        return f;
+    }
+
+    f.toString = function () {
+        return currentSum;
+    };
+
+    return f;
+}
+
+console.log(sum(0)(1)(2)(3)(4)(5)(6)); // 15
