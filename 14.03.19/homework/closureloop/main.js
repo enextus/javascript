6c4ce8a8607b55d1f8197367f91083ba179e5bf7
enextus <enextus@gmail.com>
2019-03-17 18:03:40 +0300

    sum function

--- 14.03.19/homework/closureloop/main.js
+++ 14.03.19/homework/closureloop/main.js
@@ -1,25 +1,16 @@
 function sum(firstOperand) {
 
     var currentSum = firstOperand;
-    console.log("1. currentSum = " + currentSum);
 
-    function f(secondOperand) {
-
+    function funcSum(secondOperand) {
         currentSum += secondOperand;
-        console.log("2. currentSum = " + currentSum);
-        console.log("2. f = " + f);
-
-        return f;
+        return funcSum;
     }
 
-    f.toString = function () {
-
-        console.log("3. currentSum = " + currentSum);
+    funcSum.toString = function () {
         return currentSum;
     };
-    console.log("3. f = " + f);
-    return f;
+
+    return funcSum;
 }
 
-console.log(sum(0)(1)(2)(3)(4)(5));
-console.log(sum(1)(2)(5)(44));
