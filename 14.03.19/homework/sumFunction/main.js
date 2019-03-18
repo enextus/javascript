e400d325f5afe60f4b7ef74cc13d6d3f3a95eed0
enextus <enextus@gmail.com>
2019-03-18 09:48:32 +0300

    sumFunction 8

--- 14.03.19/homework/sumFunction/main.js
+++ 14.03.19/homework/sumFunction/main.js
@@ -1,41 +1,20 @@
 function sumFunction(operator) {
 
-    console.log("operator = " + operator);
-
     var currentSum = operator;
-    console.log("0. currentSum = " + currentSum);
-
-    // ..................................................
 
     function f(parameter) {
-
-        console.log("parameter = " + parameter);
 
         if (parameter === undefined) {
             return "Result = " + currentSum;
         }
 
-        console.log("1a. currentSum = " + currentSum);
         currentSum += parameter;
-        console.log("1b. currentSum = " + currentSum);
-
-        console.log("f = " + f);
-
         return f;
     }
-
-    // ..................................................
 
     f.toString = function () {
         return currentSum;
     };
 
-
-    console.log("\n you are here \n\n\n");
-
     return f;
 }
-
-// function isObject(obj) {
-//     return (typeof obj === "object" && obj !== null) || typeof obj === "function";
-// }
