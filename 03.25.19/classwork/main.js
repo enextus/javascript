7394ff17854d42a247ac6d657936b2f89371ce63
enextus <enextus@gmail.com>
2019-04-03 15:48:24 +0300

    some changes

--- 03.25.19/classwork/main.js
+++ 03.25.19/classwork/main.js
@@ -1,45 +1,91 @@
 function createCalculator() {
-    var calculation = 0;
-  
-    var calculator = {
-      sum: function(num1, num2) {
-        if (typeof num1 !== 'number' || isNaN(num1)) {
-          throw new Error('Incorrect data type of num1');
-        }
-  
-        if (typeof num2 !== 'number' || isNaN(num2)) {
-          throw new Error('Incorrect data type of num2');
-        }
-  
-        calculation = num1 + num2;
-        return calculation;
-      },
-      diff: function(num1, num2) {
-  
-      },
-      multiplication: function(num1, num2) {
-  
-      },
-      division: function(num1, num2) {
-  
-      },
-      getCalculation: function() {
-        console.log(calculation);
-        return calculation;
-      },
-    };
-  
-    return calculator;
+  var calculation;
+
+  if (calculation === undefined) {
+    calculation = 0;
   }
-  
-  var calculator = createCalculator();
-  
-  try {
-    calculator.sum(42, 13);
-    calculator.getCalculation();
-    calculator.sum(13);
-  } catch (e) {
-    console.log('Handled error: ', e.message);
-  } finally {
-    console.log('FINALLY!');
-  }
+
+  var calculator = {
+    sum: function (num1, num2) {
+      if (typeof num1 !== 'number' || isNaN(num1)) {
+        throw new Error('Incorrect data type of num1');
+      }
+
+      if (typeof num2 !== 'number' || isNaN(num2)) {
+        throw new Error('Incorrect data type of num2');
+      }
+
+      calculation = num1 + num2;
+      return calculation;
+    },
+    diff: function (num1, num2) {
+      if (typeof num1 !== 'number' || isNaN(num1)) {
+        throw new Error('Incorrect data type of num1');
+      }
+
+      if (typeof num2 !== 'number' || isNaN(num2)) {
+        throw new Error('Incorrect data type of num2');
+      }
+
+      calculation = num1 - num2;
+      return calculation;
+    },
+    multiplication: function (num1, num2) {
+      if (typeof num1 !== 'number' || isNaN(num1)) {
+        throw new Error('Incorrect data type of num1');
+      }
+
+      if (typeof num2 !== 'number' || isNaN(num2)) {
+        throw new Error('Incorrect data type of num2');
+      }
+
+      calculation = num1 * num2;
+      return calculation;
+    },
+    division: function (num1, num2) {
+      if (typeof num1 !== 'number' || isNaN(num1)) {
+        throw new Error('Incorrect data type of num1');
+      }
+
+      if (typeof num2 !== 'number' || isNaN(num2)) {
+        throw new Error('Incorrect data type of num2');
+      }
+
+      calculation = num1 / num2;
+      return calculation;
+    },
+    getCalculation: function () {
+      return calculation;
+    },
+    showCalculation: function () {
+      console.log(calculation);
+      return calculation;
+    },
+  };
+
+  return calculator;
+}
+
+var calculator = createCalculator();
+
+var firstOperand;
+var secondOperand;
+
+try {
+  firstOperand = parseInt(prompt("Please input a first operand here:"), 10);
+} catch (e) {
+  alert('Handled error: ', e.message);
+} finally {
+  console.log('--- ok ---');
+}
+
+try {
+  secondOperand = parseInt(prompt("Please input a second operand here:"), 10);
+} catch (e) {
+  alert('Handled error: ', e.message);
+} finally {
+  console.log('--- ok ---');
+}
+
+calculator.diff(firstOperand, secondOperand);
+calculator.showCalculation();
